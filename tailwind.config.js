f17bdf054e245ebc154ac82947a77461fb615b3f
mycelgwapa <156905174+mycelgwapa@users.noreply.github.com>
2024-03-17 07:46:33 +0000

    updated 2x2

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // Specify the root directory for your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
